e44691f9e5f9457f829c05b8ce610b0683436687
joaofonsec <113640063+joaofonsec@users.noreply.github.com>
2022-09-22 17:37:39 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,42 @@
-basic.forever(function () {
-	
+let P2 = 0
+let P1 = 0
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("GO!!!")
+    basic.pause(randint(1000, 5000))
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showLeds(`
+        . . # # #
+        . . # # #
+        . . # # #
+        . . . # #
+        . . . # #
+        `)
+    basic.showString("GOL!!")
+    basic.clearScreen()
+    P2 += 1
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # # . .
+        # # . . .
+        # # . . .
+        `)
+    basic.showString("GOL!!")
+    basic.clearScreen()
+    P1 += 1
 })
